df699c7e7e0e09faff5e5b5a748fe91644bc779b
Jomar Santos <jomaroliversantos@gmail.com>
2018-05-07 21:34:51 -0700

    yaml for config

--- src/server/main.py
+++ src/server/main.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import yaml
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello, Worlds!"
+
+with open("./config.yaml", 'r') as stream:
+    try:
+        config = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=4000)
+    app.run(
+        host=config['server']['host'],
+        debug=config['server']['debug'],
+        port=config['server']['port']
+    )
