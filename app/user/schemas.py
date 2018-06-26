971164082748459d6559ca78b7efc06f554d3752
Jomar Santos <jomaroliversantos@gmail.com>
2018-06-25 23:01:55 -0700

    email validation

--- app/user/schemas.py
+++ app/user/schemas.py
@@ -1,11 +1,11 @@
 from app.main import ma
 from app.user import User
-from marshmallow import post_load
+from marshmallow import post_load, fields
 
 class UserSchema(ma.ModelSchema):
     class Meta:
         model = User
-
+    email = fields.Email()
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
