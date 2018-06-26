e0d83f95dab7ae5966e2d44d2d6c70acd70faf87
Jomar Santos <jomaroliversantos@gmail.com>
2018-06-25 22:59:39 -0700

    remove unnecessary post_load in user schema

--- app/user/schemas.py
+++ app/user/schemas.py
@@ -6,13 +6,6 @@ class UserSchema(ma.ModelSchema):
     class Meta:
         model = User
 
-    @post_load
-    def make_instance(self, data):
-        try:
-            return User(**data)
-        except Exception as e:
-            print('Bad data for User: {}'.format(data))
-            raise e
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
